b9e88bb5e8433e701a2f03b3f7d698196ddb977d
paco <francisco.sucre@cobee.io>
2024-09-13 15:15:41 +0200

    all test working with good performance

--- src/test/java/com/smes/smes/AbstractBaseIntegrationTest.java
+++ src/test/java/com/smes/smes/AbstractBaseIntegrationTest.java
@@ -1,34 +1,50 @@
 package com.smes.smes;
 
 import com.smes.smes.configs.SmesApplicationConfiguration;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@Testcontainers
 @SpringBootTest(classes = SmesApplicationConfiguration.class)
 public abstract class AbstractBaseIntegrationTest {
 
-    @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0");
-    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @ServiceConnection
+    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0").withReuse(true);
+    @ServiceConnection
+    public static final PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine")).withReuse(true);
 
     @DynamicPropertySource
     static void containersProperties(DynamicPropertyRegistry registry) {
-        mongoDBContainer.withReuse(true).start();
+
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
         registry.add("spring.data.mongodb.host", mongoDBContainer::getHost);
         registry.add("spring.data.mongodb.port", mongoDBContainer::getFirstMappedPort);
         registry.add("spring.data.mongodb.database", () -> "testdb");
 
-        postgreSQLContainer.withReuse(true).start();
         registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
         registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
         registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
     }
+
+    static {
+        mongoDBContainer.start();
+        postgreSQLContainer.start();
+    }
+
+    @AfterEach()
+    public void afterEach(){
+        mongoTemplate.getDb().drop();
+    }
+
+
 }
--- src/test/java/com/smes/smes/AbstractBaseUnitTest.java
+++ src/test/java/com/smes/smes/AbstractBaseUnitTest.java
@@ -5,15 +5,7 @@ import com.smes.smes.configs.PostgresConfiguration;
 import com.smes.smes.configs.SmesApplicationConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-@Testcontainers
 @SpringBootTest(classes = SmesApplicationConfiguration.class)
 @EnableAutoConfiguration(exclude={MongoConfiguration.class, PostgresConfiguration.class})
 public abstract class AbstractBaseUnitTest {
